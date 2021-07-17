8c6932ceb3b5f8b196dbaddee7205b99839dc51b
tusharapplore <tusharmaity989@gmail.com>
2021-07-17 20:42:30 +0530

    header left basic

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import styled from "styled-components"
+import SearchIcon from '@material-ui/icons/Search';
 
 const Header = () => {
     return (
         <HeaderContainer>
             <h1>I am the header</h1>
+            <HeaderLeft>
+                <img src="" alt="/"/>
+                <HeaderSearch>
+                    <SearchIcon />
+                    <input type="text"/>
+                </HeaderSearch>
+            </HeaderLeft>
+            <HeaderRight>
+
+            </HeaderRight>
         </HeaderContainer>
     )
 }
 
 const HeaderContainer = styled.div``;
+
+const HeaderLeft = styled.div``;
+
+const HeaderSearch = styled.div``;
+
+const HeaderRight = styled.div``;
 
 export default Header
