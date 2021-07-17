724d0f0601b3a44a393a0d4b10fe4fc422eef0f4
tusharapplore <tusharmaity989@gmail.com>
2021-07-17 20:26:59 +0530

    basic setup & clean up with redux template

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from "styled-components"
+import Header from './Header';
+
+function App() {
+  return (
+    <AppContainer>
+      <Header />
+    </AppContainer>
+  );
+}
+
+const AppContainer = styled.div``;
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from "styled-components"
+
+const Header = () => {
+    return (
+        <HeaderContainer>
+            <h1>I am the header</h1>
+        </HeaderContainer>
+    )
+}
+
+const HeaderContainer = styled.div``;
+
+export default Header
