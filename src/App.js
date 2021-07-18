bc85a9dbb9a0e3da03816d5008a7de0743bd4841
tusharapplore <tusharmaity989@gmail.com>
2021-07-18 16:52:11 +0530

    Basic sidebar UI added

--- src/App.js
+++ src/App.js
@@ -14,8 +14,15 @@ function App() {
   );
 }
 
-const AppContainer = styled.div``;
+const AppContainer = styled.div`
+  background-color: #f3f2ef;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
-const AppBody = styled.div``;
+const AppBody = styled.div`
+  display: flex;
+`;
 
 export default App;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,10 +6,10 @@ const Sidebar = () => {
     return (
         <SidebarContainer>
             <SidebarTop>
-                <img src="" alt=""/>
-                <Avatar />
+                <img src="https://images.unsplash.com/photo-1533735380053-eb8d0759b24a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=673&q=80" alt=""/>
+                <Avatar className="sidebar__avatar"/>
                 <h2>Tushar Maity</h2>
-                <h4>tusharmaity989@gmail.com</h4>
+                <h4>tusharmaity578dsdagmail.com</h4>
             </SidebarTop>
 
             <SideabarStats>
@@ -23,18 +23,90 @@ const Sidebar = () => {
                     <SidebarStatNumber>2,545</SidebarStatNumber>
                 </SidebarStat>
             </SideabarStats>
+
+            <SidebarBottom>
+                <p>Recent</p>
+            </SidebarBottom>
         </SidebarContainer>
     )
 }
 
-const SidebarContainer = styled.div``;
+const SidebarContainer = styled.div`
+    top: 80px;
+    position: sticky;
+    flex: 0.2;
+    border-radius: 10px;
+    text-align: center;
+    height: fit-content;
+`;
 
-const SidebarTop = styled.div``;
+const SidebarTop = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border: 1px solid lightgray;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    border-bottom: 0;
+    background-color: white;
+    padding-bottom: 10px;
 
-const SideabarStats = styled.div``;
+    img {
+        margin-bottom: -20px;
+        width: 100%;
+        height: 60px;
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+        object-fit: cover;
+    }
 
-const SidebarStat = styled.div``;
+    h2 {
+        font-size: 18px;
+    }
 
-const SidebarStatNumber = styled.p``;
+    h4 {
+        color: gray;
+        font-size: 12px;
+    }
+    
+    .sidebar__avatar {
+        margin-bottom: 10px;
+    }
+`;
+
+const SideabarStats = styled.div`
+    padding: 10px;
+    margin-bottom: 10px;
+    border: 1px solid lightgray;
+    background-color: #fff;
+    border-bottom-right-radius: 10px;
+    border-bottom-left-radius: 10px;
+`;
+
+const SidebarStat = styled.div`
+    margin-top: 10px;
+    display: flex;
+    justify-content: space-between;
+
+    p {
+        color: gray;
+        font-size: 13px;
+        font-weight: 600;
+    }
+`;
+
+const SidebarStatNumber = styled.p`
+    font-weight: bold;
+    color: #0a66c2 !important;
+`;
+
+const SidebarBottom = styled.div`
+    text-align: left;
+    padding: 10px;
+    border: 1px solid lightgray;
+    background-color: #fff;
+    border-radius: 10px;
+    margin-top: 10px;
+`;
 
 export default Sidebar
